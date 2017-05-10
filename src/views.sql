797665a999cd17729972bb1daf6edaf04098fbf0
elenatsoi <elenatsoi@gmail.com>
2017-05-09 17:14:59 -0700

    added views.sql

---
+++ src/views.sql
@@ -0,0 +1,30 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  Elena
+ * Created: May 9, 2017
+ */
+
+-- 1. Customer_v – for each customer, indicate his or her name as well as the customer type 
+--    (prospect, steady or premier) as well as the number of years that customer has been with us.
+
+
+-- 2. Customer_addresses_v – for each customer, indicate whether they are an individual or a 
+--    corporate account, and display all of the addresses that we are managing for that customer.
+
+
+-- 3. Mechanic_mentor_v – reports all of the mentor/mentee relationships at Dave’s, sorted by the 
+--    name of the mentor, then the name of the mentee.
+
+
+-- 4. Premier_profits_v – On a year by year basis, show the premier customer’s outlay versus what 
+--    they would have been charged for the services which they received had they merely been steady customers.
+
+
+-- 5. Prospective_resurrection_v – List all of the prospective customers who have had three or more 
+--    contacts, and for whom the most recent contact was more than a year ago.  They might be ripe 
+--    for another attempt.
+
